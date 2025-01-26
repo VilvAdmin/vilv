d82b5ebd23f24eb2dd63840bdcd3bbecded9f683
Milo Wera <milowera@Milos-MacBook-Air.local>
2025-01-26 19:47:53 +0100

    top nav statistics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ function TopNav() {
   return (
     <nav className="flex items-center justify-between w-full text-center p-4 text-xl font-semibold px-16 ">
       <img src="./Vilv_no_bg.png"/>
+      <div id="stats-container">
+        <iframe src="https://www.vvdwprojects.be/vilv/data/stats-external-global.php" width="250" height="85" title="History" style={{border: "none"}}>
+        </iframe>
+      </div>
       <div>Sign in</div>
     </nav>
   )
