5c2a0ef61a0c03ca630d6b8992fe05004ede8006
Milo Wera <milowera@Milos-MacBook-Air.local>
2025-01-28 21:31:23 +0100

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Vilv
-    </main>
+    <>
+    <h1 className="text-vilvBlue text-xl font-semibold pb-4">Vilv homepage</h1>
+    </>
   );
 }
