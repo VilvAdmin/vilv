153cd01f2e75114a7d508bf8ee734cb662c01ba5
Milo Wera <milowera@Milos-MacBook-Air.local>
2025-02-24 20:38:04 +0100

    reload issue

--- src/app/games/StatusSelect.tsx
+++ src/app/games/StatusSelect.tsx
@@ -5,42 +5,27 @@ import { useState } from "react";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select";
 import { statusEnum } from "~/server/db/schema";
 
-export default function StatusSelect( { game_id, status }: { game_id: string, status: (string | null)}) {
+export default function StatusSelect( { game_id, status }: { game_id: string, status: (string)}) {
   const { user } = useUser();
   const statusValues = Object.values(statusEnum.enumValues);
-  const [selectedStatus, setSelectedStatus] = useState(status ?? undefined);
+  const [selectedStatus, setSelectedStatus] = useState(status ?? "");
 
   const handleStatusChange = async (value: string) => {
-    if (selectedStatus === undefined) {
-      setSelectedStatus(value);
-      const response = await fetch('/api/availabilities', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-              game_id: game_id,
-              user_id: user?.id,
-              status: value,
-              player_name: user?.fullName ?? user?.username ?? user?.id
-          }),
-      });
-    }
-    else {
-      setSelectedStatus(value);
-      const response = await fetch('/api/availabilities', {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            game_id: game_id,
-            user_id: user?.id,
-            status: value,
-            player_name: user?.fullName ?? user?.username ?? user?.id
-        }),
-      });
-    }
+    setSelectedStatus(value);
+
+    const method = status === null ? 'POST' : 'PATCH';
+    const response = await fetch('/api/availabilities', {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        game_id,
+        user_id: user?.id,
+        status: value,
+        player_name: user?.fullName ?? user?.username ?? user?.id
+      }),
+    });
   }
 
   return (
