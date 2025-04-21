4479cd1bcfc8d937afc5dce0725d9ff5d237a5f3
Milo Wera <milowera@Milos-MacBook-Air.local>
2025-04-21 12:35:04 +0200

    games import spam protection

--- src/app/games-import/GamesImportForm.tsx
+++ src/app/games-import/GamesImportForm.tsx
@@ -1,31 +1,63 @@
-"use client";
+'use client';
 
-import { Input } from "~/components/ui/input";
-import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "~/components/ui/table";
-import TypeSelect from "./TypeSelect";
-import type { Game, GameImport } from "~/types";
-import { useForm } from "react-hook-form";
-import { format } from "date-fns";
+import { Input } from '~/components/ui/input';
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from '~/components/ui/table';
+import TypeSelect from './TypeSelect';
+import type { Game, GameImport } from '~/types';
+import { useForm } from 'react-hook-form';
+import { format } from 'date-fns';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function GamesImportForm({ data }: { data: GameImport[] }) {
-  const date = new Date();
-  const defaultGames: { date: string; time: string; home_team: string; away_team: string; type: "Beker" | "Vriendschappelijk" | "Competitie"; season: string }[] = data.map(game => ({
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const defaultGames: {
+    date: string;
+    time: string;
+    home_team: string;
+    away_team: string;
+    type: 'Beker' | 'Vriendschappelijk' | 'Competitie';
+    season: string;
+  }[] = data.map((game) => ({
     date: format(new Date(game.startTime), 'yyyy-MM-dd'),
     time: game.startTime.slice(11, 16),
     home_team: game.homeTeam.name,
     away_team: game.awayTeam.name,
-    type: game.series.name.includes("Beker") ? "Beker" :
-      game.series.name === "Recrea" ? "Vriendschappelijk" : "Competitie",
-    season: date.getMonth() < 5 ? `${date.getFullYear() - 1}-${date.getFullYear()}` : `${date.getFullYear()}-${date.getFullYear() + 1}`
+    type: game.series.name.includes('Beker')
+      ? 'Beker'
+      : game.series.name === 'Recrea'
+        ? 'Vriendschappelijk'
+        : 'Competitie',
+    season:
+      new Date(game.startTime).getMonth() < 7
+        ? `${new Date(game.startTime).getFullYear() - 1}-${new Date(game.startTime).getFullYear()}`
+        : `${new Date(game.startTime).getFullYear()}-${new Date(game.startTime).getFullYear() + 1}`,
   }));
 
   const form = useForm<{ games: Game[] }>({
     defaultValues: {
-      games: defaultGames
-    }
+      games: defaultGames,
+    },
   });
 
   const onSubmit = async ({ games }: { games: Game[] }) => {
+    setLoading(true);
     try {
       const res = await fetch('/api/games', {
         method: 'POST',
@@ -33,17 +65,52 @@ export default function GamesImportForm({ data }: { data: GameImport[] }) {
         body: JSON.stringify(games),
       });
       form.reset();
+      setLoading(false);
+      router.push('/games');
     } catch (error: unknown) {
       console.error('Failed to add game:', error);
     }
-  }
+  };
 
   return (
     <>
       <form onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="flex justify-between items-center pb-4">
-          <h1 className="text-vilvBlue text-xl font-semibold pb-4">Inschrijven op wedstrijden</h1>
-          <button type="submit" className="bg-vilvGreen text-white p-2 rounded">Importeer</button>
+        <div className="flex items-center justify-between pb-4">
+          <h1 className="pb-4 text-xl font-semibold text-vilvBlue">Inschrijven op wedstrijden</h1>
+
+          <Dialog>
+            <DialogTrigger asChild>
+              <button className="rounded-md bg-vilvGreen p-2 text-white">Importeer</button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md">
+              <DialogHeader>
+                <DialogTitle className="text-vilvBlue">Wedstrijden importeren</DialogTitle>
+              </DialogHeader>
+              <p>
+                Weet je zeker dat je al deze wedstrijden wilt importeren? We kunnen dit niet
+                ongedaan maken.
+              </p>
+              <div className="flex justify-end space-x-4">
+                <button
+                  className="rounded-md bg-vilvGreen p-2 text-white"
+                  type="submit"
+                  onClick={form.handleSubmit(onSubmit)}
+                  disabled={loading}
+                >
+                  Importeren
+                </button>
+                <DialogClose asChild>
+                  <button className="rounded-md bg-vilvRed p-2 text-white" disabled={loading}>
+                    Annuleer
+                  </button>
+                </DialogClose>
+              </div>
+            </DialogContent>
+          </Dialog>
+
+          {/* <button type="submit" className="rounded bg-vilvGreen p-2 text-white">
+            Importeer
+          </button> */}
         </div>
         <Table>
           <TableHeader>
@@ -88,15 +155,13 @@ export default function GamesImportForm({ data }: { data: GameImport[] }) {
                 <TableCell>
                   <TypeSelect
                     status={form.watch(`games.${index}.type`)}
-                    onChange={(value: "Beker" | "Vriendschappelijk" | "Competitie") =>
-                      form.setValue(`games.${index}.type` as const, value)}
+                    onChange={(value: 'Beker' | 'Vriendschappelijk' | 'Competitie') =>
+                      form.setValue(`games.${index}.type` as const, value)
+                    }
                   />
                 </TableCell>
                 <TableCell className="w-[8rem]">
-                  <Input
-                    {...form.register(`games.${index}.season`)}
-                    defaultValue={game.season}
-                  />
+                  <Input {...form.register(`games.${index}.season`)} defaultValue={game.season} />
                 </TableCell>
               </TableRow>
             ))}
@@ -104,6 +169,5 @@ export default function GamesImportForm({ data }: { data: GameImport[] }) {
         </Table>
       </form>
     </>
-
   );
 }
--- src/app/games/[id]/GameHeader.tsx
+++ src/app/games/[id]/GameHeader.tsx
@@ -1,11 +1,17 @@
-"use client";
-import { useUser } from "@clerk/nextjs";
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "~/components/ui/dialog";
-import GameForm from "../GameForm";
-import type { Game } from "~/types";
-import { useRouter } from "next/navigation";
-import { DialogClose } from "@radix-ui/react-dialog";
-import { useState } from "react";
+'use client';
+import { useUser } from '@clerk/nextjs';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog';
+import GameForm from '../GameForm';
+import type { Game } from '~/types';
+import { useRouter } from 'next/navigation';
+import { DialogClose } from '@radix-ui/react-dialog';
+import { useState } from 'react';
 
 interface GameHeaderProps {
   game: Game;
@@ -17,7 +23,7 @@ export default function GameHeader({ game }: GameHeaderProps) {
   const router = useRouter();
 
   const userRoles = user?.publicMetadata?.roles as string[] | undefined;
-  const isAdmin = userRoles?.includes("admin");
+  const isAdmin = userRoles?.includes('admin');
 
   const deleteGame = async () => {
     try {
@@ -30,52 +36,62 @@ export default function GameHeader({ game }: GameHeaderProps) {
       } else {
         console.error('Failed to delete game:', res.statusText);
       }
-
     } catch (error: unknown) {
       console.error('Failed to delete game:', error);
     }
-  }
+  };
 
   const handleSuccess = () => {
     setDialogOpen(false);
     router.refresh();
-  }
+  };
 
   return (
-    <div className="flex justify-between items-center">
-      <h1 className="text-vilvBlue text-xl font-semibold pb-4">Wedstrijddetails</h1>
+    <div className="flex items-center justify-between">
+      <h1 className="pb-4 text-xl font-semibold text-vilvBlue">Wedstrijddetails</h1>
 
-      {isAdmin &&
+      {isAdmin && (
         <div className="flex space-x-4">
           <Dialog>
             <DialogTrigger asChild>
-              <button className="bg-vilvRed text-white p-2 rounded-md">Verwijderen</button>
+              <button className="rounded-md bg-vilvRed p-2 text-white">Verwijderen</button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-md">
               <DialogHeader>
-                <DialogTitle className="text-vilvBlue">Wedstrijd Verwijderen</DialogTitle>
+                <DialogTitle className="text-vilvBlue">Wedstrijd verwijderen</DialogTitle>
               </DialogHeader>
-              <p>Weet je zeker dat je deze wedstrijd wil verwijderen? We kunnen dit niet ongedaan maken.</p>
-              <div className="flex space-x-4 justify-end">
-                <button className="bg-vilvRed text-white p-2 rounded-md" onClick={deleteGame}>Verwijder</button>
+              <p>
+                Weet je zeker dat je deze wedstrijd wil verwijderen? We kunnen dit niet ongedaan
+                maken.
+              </p>
+              <div className="flex justify-end space-x-4">
+                <button className="rounded-md bg-vilvRed p-2 text-white" onClick={deleteGame}>
+                  Verwijder
+                </button>
                 <DialogClose asChild>
-                  <button className="bg-vilvGreen text-white p-2 rounded-md">Annuleer</button>
+                  <button className="rounded-md bg-vilvGreen p-2 text-white">Annuleer</button>
                 </DialogClose>
               </div>
             </DialogContent>
           </Dialog>
           <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
             <DialogTrigger asChild>
-              <button className="bg-vilvGreen text-white p-2 rounded-md">Aanpassen</button>
+              <button className="rounded-md bg-vilvGreen p-2 text-white">Aanpassen</button>
             </DialogTrigger>
             <DialogContent className="sm:max-w-md">
               <DialogHeader>
                 <DialogTitle className="text-vilvBlue">Wedstrijd aanpassen</DialogTitle>
               </DialogHeader>
-              <GameForm game={{ ...game, date: new Date(game.date) }} onSuccess={handleSuccess} method="PATCH" game_id={game.id} />
+              <GameForm
+                game={{ ...game, date: new Date(game.date) }}
+                onSuccess={handleSuccess}
+                method="PATCH"
+                game_id={game.id}
+              />
             </DialogContent>
           </Dialog>
-        </div>}
+        </div>
+      )}
     </div>
-  )
+  );
 }
