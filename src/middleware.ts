1138a9470ff87d8000e1bc80465a1e617ea142d7
Milo Wera <milo.wera.dev+vilv@gmail.com>
2025-06-11 22:25:40 +0200

    Use proxy for Clerk Prod - middleware changes - remove any types

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,12 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-function proxyMiddleware(req: any) {
+function proxyMiddleware(req: NextRequest) {
   if (req.nextUrl.pathname.match('__clerk')) {
     const proxyHeaders = new Headers(req.headers);
     proxyHeaders.set('Clerk-Proxy-Url', process.env.NEXT_PUBLIC_CLERK_PROXY_URL || '');
     proxyHeaders.set('Clerk-Secret-Key', process.env.CLERK_SECRET_KEY || '');
-    if (req.ip) {
-      proxyHeaders.set('X-Forwarded-For', req.ip);
-    } else {
-      proxyHeaders.set('X-Forwarded-For', req.headers.get('X-Forwarded-For') || '');
-    }
+    proxyHeaders.set('X-Forwarded-For', req.headers.get('X-Forwarded-For') || '');
 
     const proxyUrl = new URL(req.url);
     proxyUrl.host = 'frontend-api.clerk.dev';
