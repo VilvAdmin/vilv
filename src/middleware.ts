a746634afb5ab16f518e94d1c11eb241f2adb66a
Milo Wera <milo.wera.dev+vilv@gmail.com>
2025-06-11 22:23:01 +0200

    Use proxy for Clerk Prod - middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,51 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+function proxyMiddleware(req: any) {
+  if (req.nextUrl.pathname.match('__clerk')) {
+    const proxyHeaders = new Headers(req.headers);
+    proxyHeaders.set('Clerk-Proxy-Url', process.env.NEXT_PUBLIC_CLERK_PROXY_URL || '');
+    proxyHeaders.set('Clerk-Secret-Key', process.env.CLERK_SECRET_KEY || '');
+    if (req.ip) {
+      proxyHeaders.set('X-Forwarded-For', req.ip);
+    } else {
+      proxyHeaders.set('X-Forwarded-For', req.headers.get('X-Forwarded-For') || '');
+    }
+
+    const proxyUrl = new URL(req.url);
+    proxyUrl.host = 'frontend-api.clerk.dev';
+    proxyUrl.port = '443';
+    proxyUrl.protocol = 'https';
+    proxyUrl.pathname = proxyUrl.pathname.replace('/__clerk', '');
+
+    return NextResponse.rewrite(proxyUrl, {
+      request: {
+        headers: proxyHeaders,
+      },
+    });
+  }
+
+  return null;
+}
+
+const clerkHandler = clerkMiddleware();
+
+export default function middleware(req: NextRequest, event: NextFetchEvent) {
+  // First check if it's a proxy request
+  const proxyResponse = proxyMiddleware(req);
+  if (proxyResponse) {
+    return proxyResponse;
+  }
+
+  // Otherwise, use Clerk's middleware
+  return clerkHandler(req, event);
+}
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Always run for API routes AND anything passed through the proxy
+    '/(api|trpc|__clerk)(.*)',
   ],
 };
