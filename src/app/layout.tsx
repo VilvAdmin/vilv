0c69d010da61e2375b5940fceb9f90c725695f3b
Milo Wera <milo.wera.dev+vilv@gmail.com>
2025-06-11 22:15:30 +0200

    Use proxy for Clerk Prod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,30 @@
-import "~/styles/globals.css";
+import '~/styles/globals.css';
 
-import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
-import { ClerkProvider } from '@clerk/nextjs'
-import { TopNav } from "./_components/TopNav";
-import { SideNav, SideNavMobile } from "./_components/SideNav";
+import { GeistSans } from 'geist/font/sans';
+import { type Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import { TopNav } from './_components/TopNav';
+import { SideNav, SideNavMobile } from './_components/SideNav';
 
 export const metadata: Metadata = {
-  title: "FC Vilv Heverlee",
-  description: "",
-  icons: [{ rel: "icon", url: "/VilvLogo.png" }],
+  title: 'FC Vilv Heverlee',
+  description: '',
+  icons: [{ rel: 'icon', url: '/VilvLogo.png' }],
 };
 
-
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider proxyUrl="https://fcvilvheverlee.com/__clerk">
       <html lang="en" className={`${GeistSans.variable}`}>
-        <body className="flex flex-col h-screen">
+        <body className="flex h-screen flex-col">
           <TopNav />
-          <div className="hidden lg:flex flex-1">
+          <div className="hidden flex-1 lg:flex">
             <SideNav />
-            <main className="w-4/5 p-4 flex-1 flex-col">
-              {children}
-            </main>
+            <main className="w-4/5 flex-1 flex-col p-4">{children}</main>
           </div>
-          <div className="flex lg:hidden flex-col">
+          <div className="flex flex-col lg:hidden">
             <SideNavMobile />
-            <main className="w-full p-4 flex-1 flex-col">
-              {children}
-            </main>
+            <main className="w-full flex-1 flex-col p-4">{children}</main>
           </div>
         </body>
       </html>
