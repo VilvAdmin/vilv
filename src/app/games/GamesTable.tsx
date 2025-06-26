78ef2e919d168235ee99f87f6149b5660c8e9d91
Milo Wera <milo.wera.dev+vilv@gmail.com>
2025-06-26 20:46:47 +0200

    empty state for Wedstrijden

--- src/app/games/GamesTable.tsx
+++ src/app/games/GamesTable.tsx
@@ -106,45 +106,49 @@ export default function GamesTable({ games }: { games: MyGame[] }) {
         </div>
       </div>
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-vilvBlue">Datum</TableHead>
-            <TableHead className="text-vilvBlue">Uur</TableHead>
-            <TableHead className="text-vilvBlue">Thuisploeg</TableHead>
-            <TableHead className="text-vilvBlue">Uitploeg</TableHead>
-            <TableHead className="hidden text-vilvBlue md:table-cell">Type wedstrijd</TableHead>
-            <TableHead className="text-vilvBlue">Status</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {displayedGames?.map((game) => (
-            <TableRow key={game.games.id}>
-              <TableCell onClick={() => router.push(`/games/${game.games.id}`)}>
-                {game.games.date}
-              </TableCell>
-              <TableCell onClick={() => router.push(`/games/${game.games.id}`)}>
-                {game.games.time.slice(0, 5)}
-              </TableCell>
-              <TableCell onClick={() => router.push(`/games/${game.games.id}`)}>
-                {game.games.home_team}
-              </TableCell>
-              <TableCell onClick={() => router.push(`/games/${game.games.id}`)}>
-                {game.games.away_team}
-              </TableCell>
-              <TableCell
-                className="hidden md:table-cell"
-                onClick={() => router.push(`/games/${game.games.id}`)}
-              >
-                {game.games.type}
-              </TableCell>
-              <TableCell>
-                <StatusSelect status={game.status} game_id={game.games.id} />
-              </TableCell>
+      {displayedGames?.length > 0 ? (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="text-vilvBlue">Datum</TableHead>
+              <TableHead className="text-vilvBlue">Uur</TableHead>
+              <TableHead className="text-vilvBlue">Thuisploeg</TableHead>
+              <TableHead className="text-vilvBlue">Uitploeg</TableHead>
+              <TableHead className="hidden text-vilvBlue md:table-cell">Type wedstrijd</TableHead>
+              <TableHead className="text-vilvBlue">Status</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {displayedGames?.map((game) => (
+              <TableRow key={game.games.id}>
+                <TableCell onClick={() => router.push(`/games/${game.games.id}`)}>
+                  {game.games.date}
+                </TableCell>
+                <TableCell onClick={() => router.push(`/games/${game.games.id}`)}>
+                  {game.games.time.slice(0, 5)}
+                </TableCell>
+                <TableCell onClick={() => router.push(`/games/${game.games.id}`)}>
+                  {game.games.home_team}
+                </TableCell>
+                <TableCell onClick={() => router.push(`/games/${game.games.id}`)}>
+                  {game.games.away_team}
+                </TableCell>
+                <TableCell
+                  className="hidden md:table-cell"
+                  onClick={() => router.push(`/games/${game.games.id}`)}
+                >
+                  {game.games.type}
+                </TableCell>
+                <TableCell>
+                  <StatusSelect status={game.status} game_id={game.games.id} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <p>Er zijn nog geen wedstrijden voor dit seizoen.</p>
+      )}
     </>
   );
 }
